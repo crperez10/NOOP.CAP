6e6ecb06347a082edd6f67f9d0cb80fc5ba2d592
Cristian Perez <cr.perezpinci@gmail.com>
2026-05-02 15:06:12 -0300

    Actualización

--- api/auth/index.js
+++
@@ -1,7 +0,0 @@
-import { runExpress } from "../_express.js";
-
-export default async function handler(req, res) {
-  const queryIndex = req.url.indexOf("?");
-  const query = queryIndex >= 0 ? req.url.slice(queryIndex) : "";
-  return runExpress(req, res, `/auth${query}`);
-}
--- api/health.js
+++
@@ -1,5 +0,0 @@
-import { runExpress } from "./_express.js";
-
-export default async function handler(req, res) {
-  return runExpress(req, res, "/api/health");
-}
